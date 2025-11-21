a47aa10581f3cd38ab6e14a7226e5521631b0cee
pramod <yewalepramod50@gmail.com>
2025-11-21 16:09:12 +0530

    json example

---
+++ playjson.js
@@ -0,0 +1,25 @@
+let studentdata = [
+    {
+        "name":"John Doe",
+        "rollno":23,
+        "calss":"MCA Distinction",
+        "address":{
+            "city":"New York",
+            "state":"NY",
+            "country":"USA"
+        },
+        marks:[85, 90, 78, 92]
+    },
+    {
+        "name":"Jane Smith",
+        "rollno":45,
+        "calss":"MCA First Class",
+        "address":{
+            "city":"Los Angeles",
+            "state":"CA",
+            "country":"USA"
+        },
+        marks:[88, 76, 91, 85]
+    }
+];
+console.log(studentdata)
