8d6523914fee7dc489880fc0030b4e03aa8ceb43
pramod <yewalepramod50@gmail.com>
2025-11-22 13:22:30 +0530

    string function

---
+++ stringfunction.js
@@ -0,0 +1,9 @@
+//string function
+let str = "Hello, World!";
+console.log(str.length); // Output: 13
+console.log(str.toUpperCase()); // Output: "HELLO, WORLD!"
+console.log(str.toLowerCase()); // Output: "hello, world!"
+console.log(str.indexOf("World")); // Output: 7
+console.log(str.slice(0, 5)); // Output: "Hello"
+
+//array function
